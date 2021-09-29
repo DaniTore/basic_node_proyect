998d85fde693cbe0956d42be3fb7f4ce2fe79eea
dToreMittendofer <d.tore.mittendorfer@accenture.com>
2021-09-29 19:15:47 +0200

    create helper and 404 page

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
+const rootDir = require('../utils/path');
+
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('get /');
-    res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router
